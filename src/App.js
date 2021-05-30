837e79a0e7c0ff15bb944f5335639923110a9240
abhishek <kumarabhishekranjancbsa@gmail.com>
2021-05-30 22:31:43 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <p>This is my first React app</p>
+      <p>This is my first React app and first commit</p>
     </div>
   );
 }
